185e5b7274eb268c1b37afca84220354135b4ece
Jacek Laskowski <jacek@japila.pl>
2022-03-30 13:02:55 +0200

    Refactor

--- src/main/scala/Exercise.scala
+++ src/main/scala/Exercise.scala
@@ -1,20 +1,15 @@
-import scala.collection.mutable.ListBuffer
-
-class Exercise {
-  def sumPrevElemIsEqual(ls :List[Int], n: Int)={
-    if(n > 0 && ls.nonEmpty){
-    val lsGrouped = ls.grouped(n)
-    val checkList = new ListBuffer[Boolean]
-    lsGrouped.foreach(x => {
-      if(x.length == n){
-        val res = x.sum - x(n-1)
-        val z = res == x(n-1)
-        checkList.addOne(z)
-      }
-    })
-    !checkList.contains(false)
-    }else{
-      false
+object Exercise {
+  // Multiple argument list
+  // Max: 2
+  def sumPrevElemIsEqual(ls: Seq[Int], n: Int) = {
+    (n, ls) match {
+      case _ if n > 0 && ls.nonEmpty =>
+        ls.grouped(n)
+          .filter(ns => ns.length == n).forall { ns =>
+          val res = ns.sum - ns(n - 1)
+          res == ns(n - 1)
+        }
+      case _ => false
     }
   }
 }
